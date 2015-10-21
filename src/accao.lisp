a151e29d57fd05c9e538cd871c14ef562f09d625
micaelbatista <ist178941@lab8p1.rnl.tecnico.ulisboa.pt>
2015-10-21 14:24:23 +0100

    accao.lisp modified

--- src/accao.lisp
+++ src/accao.lisp
@@ -1,3 +1,26 @@
-(defun cria-accao (coluna peca) (cons coluna peca))
-(defun accao-coluna (accao) (car accao))
-(defun accao-peca (accao) (cdr accao))
+;; TIPO ACCAO
+
+;; cria-accao: inteiro x array -> accao
+;; Este construtor recebe um inteiro correspondente à posição da coluna mais à 
+;; esquerda a partir da qual a peça vai ser colocada, e um array com a 
+;; configuração da peça a colocar, e devolve uma nova acção
+
+(defun cria-accao (coluna peca)
+	(cons coluna peca)
+)
+
+;; accao-coluna: accao -> inteiro
+;; Este selector devolve um  inteiro  correspondente  à coluna mais  à  esquerda 
+;; a  partir  da  qual  a peça vai ser colocada.
+ 
+(defun accao-coluna (accao)
+	(car accao)
+)
+
+;; accao-peca: accao -> array
+;; Este selector devolve o array com a configuração geométrica exacta com que 
+;; vai ser colocada.
+
+(defun accao-peca (accao)
+	(cdr accao)
+)
