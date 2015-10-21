193c04969d742c03d1b5c68ece5183329c10f7e2
micaelbatista <ist178941@lab8p1.rnl.tecnico.ulisboa.pt>
2015-10-21 15:04:12 +0100

    estado.lisp added

---
+++ src/estado.lisp
@@ -0,0 +1,45 @@
+;; TIPO ESTADO
+
+;; Estrutura do estado do jogo
+;; - pontos: o número de pontos conseguidos até ao momento no jogo.
+;; - pecas-por-colocar: uma  lista  com  as  peças  que  ainda  estão por
+;;     colocar,  pela  ordem  de colocação. As peças nesta lista são
+;;     representadas pelo símbolo correspondente à letra da peça.
+;; - pecas-colocadas: uma  lista  com  as  peças  já  colocadas  no  tabuleiro.
+;;		 Esta lista deve encontrar-se ordenada da peça mais recente para a mais antiga.
+;; - tabuleiro: o tabuleiro com as posições actualmente preenchidas do jogo.
+
+(defstruct estado pontos pecas-por-colocar pecas-colocadas tabuleiro)
+
+;; copia-estado: estado -> estado
+;; Este construtor recebe um estado e devolve um novo estado cujo conteúdo deve 
+;; ser copiado a partir  do  estado  original.  O  estado devolvido deverá
+;; garantir  que  qualquer  alteração  feita  ao estado original não deve ser 
+;; repercutida no novo estado e vice-versa.
+
+(defun copia-estado (estado)
+  (copy-estado estado)
+)
+
+;; estados-iguais-p: estado x estado -> logico
+;; Este teste recebe dois estados, e devolve o valor lógico verdade se os dois
+;; estados forem iguais (i.e. tiverem o mesmo conteúdo) e falso caso contrário.
+
+(defun estados-iguais-p (estado1 estado2)
+  (and
+    (eql (estado-pontos estado1) (estado-pontos estado2))
+    (eql (estado-pecas-por-colocar estado1) (estado-pecas-por-colocar estado2))
+    (eql (estado-pecas-colocadas estado1) (estado-pecas-colocadas estado2))
+    (eql (estado-tabuleiro estado1) (estado-tabuleiro estado2))
+  )
+)
+
+;; estado-final-p: estado -> logico
+;; Este  reconhecedor  recebe  um  estado  e devolve o  valor  lógico  verdade
+;; se  corresponder  a  um estado final onde o jogador já não possa fazer mais
+;; jogadas e falso caso contrário. Um estado é considerado final se o tabuleiro
+;; tiver atingido o topo ou se já não existem peças por colocar.
+
+(defun estado-final-p (estado)
+  (tabuleiro-topo-preenchido-p (estado-tabuleiro estado))
+)
