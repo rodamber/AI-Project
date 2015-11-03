9a8888bd76158ae45259d8f290bd599d66fa6c30
frank178960 <franc.best@hotmail.com>
2015-11-03 15:01:27 +0000

    so falta o resultado

--- src/problema.lisp
+++ src/problema.lisp
@@ -11,7 +11,7 @@
 ;; - custo-caminho: funcao que dado um estado devolve o custo do caminho
 ;;     desde o estado inicial até esse estado.
 
-(defstruct problema estado-inicial solucao(estado) accoes(estado) resultado(estado accao) custo-caminho(estado))
+(defstruct problema estado-inicial solucao accoes resultado custo-caminho)
 
 ;;	solucao: estado -> logico
 ;;	Esta função recebe um estado, e devolve o valor logico verdade se o estado
@@ -29,43 +29,42 @@
 ;;Uma acção e considerada valida mesmo que faça o jogador perder o jogo.
 
 (defun accoes (estado)
-	(let ((lista-accoes nil))
-	(let ((n 9))
-	(cond((and (car (estado-pecas-por-colocar estado) )(= 'i))
-		(dotimes (i n)
-			(push (cria-accao (i peca-i0)) lista-accoes))
-		(dotimes (i n-3)
-			(push (cria-accao (i peca-i1)) lista-accoes))))
-	(cond((and (car (estado-pecas-por-colocar estado) )(= 'l))
-		(dotimes (i n-1)
-			(push (cria-accao (i peca-l0)) lista-accoes))
-		(dotimes (i n-3)
-			(push (cria-accao (i peca-l1)) lista-accoes))
-		(dotimes (i n-1)
-			(push (cria-accao (i peca-l2)) lista-accoes))
-		(dotimes (i n-3)
-			(push (cria-accao (i peca-l3)) lista-accoes))))
-	(cond((and (car (estado-pecas-por-colocar estado) )(= 'o))
-		(dotimes (i n-2)
-			(push (cria-accao (i peca-o0)) lista-accoes))))
-	(cond((and (car (estado-pecas-por-colocar estado) )(= 's))
-		(dotimes (i n-3)
-			(push (cria-accao (i peca-s0)) lista-accoes))
-		(dotimes (i n-2)
-			(push (cria-accao (i peca-s1)) lista-accoes))))
-	(cond((and (car (estado-pecas-por-colocar estado) )(= 'z))
-		(dotimes (i n-3)
-			(push (cria-accao (i peca-z0)) lista-accoes))
-		(dotimes (i n-2)
-			(push (cria-accao (i peca-z1)) lista-accoes))))
-	(cond((and (car (estado-pecas-por-colocar estado) )(= 't))
-		(dotimes (i n-1)
-			(push (cria-accao (i peca-t0)) lista-accoes))
-		(dotimes (i n-3)
-			(push (cria-accao (i peca-t1)) lista-accoes))
-		(dotimes (i n-1)
-			(push (cria-accao (i peca-t2)) lista-accoes))))
-    (lista-accoes)))
+	(let ((lista-accoes nil) (n 9))
+	(cond((and (car (estado-pecas-por-colocar estado))(= 'i))
+			(dotimes (i n)
+				(cons (cria-accao (i peca-i0)) lista-accoes))
+			(dotimes (i n-3)
+				(cons (cria-accao (i peca-i1)) lista-accoes)))
+		 ((and (car (estado-pecas-por-colocar estado)) (= 'l))
+			(dotimes (i n-1)
+				(cons (cria-accao (i peca-l0)) lista-accoes))
+			(dotimes (i n-3)
+				(cons (cria-accao (i peca-l1)) lista-accoes))
+			(dotimes (i n-1)
+				(cons (cria-accao (i peca-l2)) lista-accoes))
+			(dotimes (i n-3)
+				(cons (cria-accao (i peca-l3)) lista-accoes)))
+		 ((and (car (estado-pecas-por-colocar estado))(= 'o))
+			(dotimes (i n-2)
+				(cons (cria-accao (i peca-o0)) lista-accoes)))
+		 ((and (car (estado-pecas-por-colocar estado))(= 's))
+			(dotimes (i n-3)
+				(cons (cria-accao (i peca-s0)) lista-accoes))
+			(dotimes (i n-2)
+				(cons (cria-accao (i peca-s1)) lista-accoes)))
+		 ((and (car (estado-pecas-por-colocar estado))(= 'z))
+			(dotimes (i n-3)
+				(cons (cria-accao (i peca-z0)) lista-accoes))
+			(dotimes (i n-2)
+				(cons (cria-accao (i peca-z1)) lista-accoes)))
+		 ((and (car (estado-pecas-por-colocar estado) )(= 't))
+			(dotimes (i n-1)
+				(cons (cria-accao (i peca-t0)) lista-accoes))
+			(dotimes (i n-3)
+				(cons (cria-accao (i peca-t1)) lista-accoes))
+			(dotimes (i n-1)
+				(cons (cria-accao (i peca-t2)) lista-accoes)))
+    (lista-accoes))))
 
 ;; 	resultado: estado x accao -> estado
 ;;	Esta funcao recebe um estado e uma accao, e devolve um novo estado que resulta
@@ -90,7 +89,7 @@
 				tabuleiro-remove-linha(estado-tabuleiro estado1 n)
 				(+ 1 count)))
 		(cond(eq (count 1))
-			(+ 100 estado-pontos estado1))
+			(incf estado-pontos estado1 (100)))
 		(cond(eq (count 2))
 			(+ 300 estado-pontos estado1))
 		(cond(eq (count 3))
@@ -100,12 +99,11 @@
 		(estado1))))))))
 
 ;;	qualidade: estado -> inteiro
-;;	Esta funcao recebe um estado e retorna um valor de qualidade que corresponde ao
+;;	Esta funcao recebe um estado e retorna um valor de quatidade que corresponde ao
 ;;valor negativo dos pontos ganhos ate ao momento.
 
 (defun quantidade (estado)
-
-)
+	(* -1 (estado-pontos estado)))
 
 ;;	custo-oportunidade: estado -> inteiro
 ;;	Esta função, dado um estado, devolve o custo de oportunidade de todas as accoes
@@ -113,5 +111,14 @@
 ;;por cada peca colocada
 
 (defun custo-oportunidade (estado)
+	(let (co (* 800 (list-length estado-pecas-colocadas estado))
+	(decf co (estado-pontos estado))
+	co)))
 
-)
+;;	custo-caminho: estado -> inteiro
+;;	Esta função, dado um estado, devolve o custo de caminho desde o estado inicial
+;;até ao estado inserido
+
+(defun custo-caminho(estado)
+	(let (cc (* 800 (list-length estado-pecas-colocadas problema-estado-inical)))
+	(decf cc (estado-pontos estado))))
