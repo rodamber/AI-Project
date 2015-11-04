981824702527dceb7d15b0e6619b07b45f39a303
micaelbatista <micael.pina@gmail.com>
2015-11-04 14:46:03 +0000

    correction in tabuleiro.lisp and modified all functions in problema.lisp. Added new utils.lisp version and merged all files in parte_1.lisp

---
+++ src/.s
@@ -0,0 +1,7 @@
+;; SETUP FILE
+
+(load "tabuleiro.lisp")
+(load "estado.lisp")
+(load "accao.lisp")
+(load "problema.lisp")
+(load "utils.lisp")
--- src/problema.lisp
+++ src/problema.lisp
@@ -23,8 +23,8 @@
 (defun solucao (estado)
   (let* ((tabuleiro  (estado-tabuleiro         estado))
          (pecas      (estado-pecas-por-colocar estado))
-         (preenchido (tabuleiro-preenchido-p   tabuleiro)))
-    (or (not preenchido) (null pecas))))
+         (preenchido (tabuleiro-topo-preenchido-p   tabuleiro)))
+    (and (not preenchido) (null pecas))))
 
 ;;	accoes: estado -> lista de accoes
 ;;	Esta funcao recebe um estado e devolve uma lista de acções correspondendo a
@@ -37,7 +37,7 @@
   	( 'i
       (dotimes (i n)
         (setf lista-accoes (cons (cria-accao i peca-i0) lista-accoes)))
-      (dotimes (i (- n 2))
+      (dotimes (i (- n 3))
         (setf lista-accoes (cons (cria-accao i peca-i1) lista-accoes))))
         
   	( 'l
@@ -52,25 +52,25 @@
       (dotimes (i (1- n))
         (setf lista-accoes(cons (cria-accao i peca-o0) lista-accoes))))
         
-    ('s
+    ( 's
       (dotimes (i (- n 2))
         (setf lista-accoes(cons (cria-accao i peca-s0) lista-accoes)))
       (dotimes (i (1- n))
         (setf lista-accoes(cons (cria-accao i peca-s1) lista-accoes))))
      
-		('z
+		( 'z
       (dotimes (i (- n 2))
         (setf lista-accoes(cons (cria-accao i peca-z0) lista-accoes)))
       (dotimes (i (1- n))
       	(setf lista-accoes(cons (cria-accao i peca-z1) lista-accoes))))
         
-    ('t
-      (dotimes (i (1- n))
-        (setf lista-accoes(cons (cria-accao (i peca-t0)) lista-accoes))
-        (setf lista-accoes(cons (cria-accao (i peca-t2)) lista-accoes)))
+    ( 't
       (dotimes (i (- n 2))
-        (setf lista-accoes(cons (cria-accao (i peca-t1)) lista-accoes))
-        (setf lista-accoes(cons (cria-accao (i peca-t3)) lista-accoes)))))
+        (setf lista-accoes(cons (cria-accao i peca-t0) lista-accoes))
+        (setf lista-accoes(cons (cria-accao i peca-t2) lista-accoes)))
+      (dotimes (i (1- n))
+        (setf lista-accoes(cons (cria-accao i peca-t1) lista-accoes))
+        (setf lista-accoes(cons (cria-accao i peca-t3) lista-accoes)))))
     lista-accoes))
     
     
@@ -82,40 +82,52 @@
 (defun resultado (estado accao)
   (setf (estado-pecas-colocadas estado) (cons (first (estado-pecas-por-colocar estado)) (estado-pecas-colocadas estado)))
   (setf (estado-pecas-por-colocar estado) (rest (estado-pecas-por-colocar estado)))
-  (dotimes (linha-tabuleiro (array-dimension (estado-tabuleiro estado) 0))
-    (if (verifica-espaco-livre (estado-tabuleiro estado) (cdr accao) (car accao) linha-tabuleiro)
-      (progn (setf (estado-tabuleiro estado) (coloca-peca (estado-tabuleiro estado) (cdr accao) (car accao) linha-tabuleiro))
-             (setf linha-tabuleiro (array-dimension (estado-tabuleiro estado) 0)))))
-  (if T                                                                                             ;; verificar se o topo do tabuleiro esta preenchido
-    (let ((linhas-completas 0)) 
-      (dotimes (altura-tabuleiro (array-dimension (estado-tabuleiro estado) 0))
-        (if NIL (incf linhas-completas)))                                                           ;; verificar se linha altura-tabuleiro do tabuleiro esta preenchida
-      (case linhas-completas
-        (1 (incf (estado-pontos estado) 100))
-        (2 (incf (estado-pontos estado) 300))
-        (3 (incf (estado-pontos estado) 500))
-        (4 (incf (estado-pontos estado) 800)))))
-  estado)
+  
+  (let* ((tabuleiro (estado-tabuleiro estado))
+        (tabuleiro-altura (array-dimension tabuleiro 0))
+        (peca (accao-peca accao))
+        (peca-coluna (accao-coluna accao))
+        (linhas-completas 0))
+        
+    (dotimes (linha-tabuleiro tabuleiro-altura)
+      (if (verifica-espaco-livre tabuleiro peca peca-coluna linha-tabuleiro)
+        (progn (setf (estado-tabuleiro estado) (coloca-peca tabuleiro peca peca-coluna linha-tabuleiro))
+               (setf linha-tabuleiro tabuleiro-altura))))
+               
+    (if (tabuleiro-topo-preenchido-p tabuleiro)
+      (return-from resultado estado))  
+      
+    (dotimes (linha-tabuleiro tabuleiro-altura)
+      (if (tabuleiro-linha-completa-p tabuleiro linha-tabuleiro)
+        (progn (incf linhas-completas)
+          (tabuleiro-remove-linha! tabuleiro linha-tabuleiro)))) 
+    (case linhas-completas
+      (1 (incf (estado-pontos estado) 100))
+      (2 (incf (estado-pontos estado) 300))
+      (3 (incf (estado-pontos estado) 500))
+      (4 (incf (estado-pontos estado) 800)))
+      
+    estado))
                                      
 (defun verifica-espaco-livre (tabuleiro peca x y)
-  (dotimes (altura (array-dimension peca 0) T)
-    (dotimes (largura (array-dimension peca 1))
-      (if (and (< (+ altura y) 18)
-               (< (+ largura x) 10)
-               (aref tabuleiro (+ altura y) (+ largura x))) 
+  (dotimes (linha (array-dimension peca 0) T)
+    (dotimes (coluna (array-dimension peca 1))
+      (if (and (< (+ linha y) (array-dimension tabuleiro 0))
+               (< (+ coluna x) (array-dimension tabuleiro 1))
+               (tabuleiro-preenchido-p tabuleiro (+ linha y) (+ coluna x))) 
         (return-from verifica-espaco-livre NIL)))))
 
 (defun coloca-peca (tabuleiro peca x y)
   (dotimes (altura-peca (array-dimension peca 0) tabuleiro)
     (dotimes (largura-peca (array-dimension peca 1))
-      (setf (aref tabuleiro (+ y altura-peca) (+ x largura-peca)) (aref peca altura-peca largura-peca)))))
+      (tabuleiro-preenche! tabuleiro (+ y altura-peca) (+ x largura-peca)))))
     
 
 ;;	qualidade: estado -> inteiro
 ;;	Esta funcao recebe um estado e retorna um valor de quatidade que corresponde ao
 ;;valor negativo dos pontos ganhos ate ao momento.
 
-(defun quantidade (estado)
+(defun qualidade (estado)
   (* -1 (estado-pontos estado)))
 
 ;;	custo-oportunidade: estado -> inteiro
@@ -123,14 +135,18 @@
 ;;realizadas ate ao momento, assumindo que é sempre possivel fazer o maximo de pontos
 ;;por cada peca colocada
 
-(defun custo-oportunidade (estado))
+(defun custo-oportunidade (estado)
+  (- (pontos-maximos(estado-pecas-colocadas estado)) (estado-pontos estado)))
+
   
-(defun pontos-maximos(lis))
+  (defun pontos-maximos (lis)
+  (cond ((equal (first lis) 'i)
+            (+ (pontos-maximos2 (rest lis)) 800))
+        ((or (equal (first lis) 'j) (equal(first lis) 'l))
+            (+ (pontos-maximos2 (rest lis)) 500))
+        ((or (equal (first lis) 's) (equal(first lis) 'z) (equal (first lis) 't) 
+             (equal(first lis) 'o))
+            (+ (pontos-maximos2 (rest lis)) 300))
+        (t 0)))
 
-;;	custo-caminho: estado -> inteiro
-;;	Esta função, dado um estado, devolve o custo de caminho desde o estado inicial
-;;até ao estado inserido
-
-(defun custo-caminho(estado)
-  (let (cc (* 800 (list-length estado-pecas-colocadas problema-estado-inicial)))
-  (decf cc (estado-pontos estado))))
+  
