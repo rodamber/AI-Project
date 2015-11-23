89220b1312e2b83d0ba0849fe0cb98a2a7b0acae
rodamber <rodrigoambernardo@gmail.com>
2015-11-23 16:01:27 +0000

    Changed comments to docstrings on {estado,problema}.lisp

--- src/problema.lisp
+++ src/problema.lisp
@@ -1,41 +1,41 @@
 ;; TIPO PROBLEMA
 
-;; Estrutura do problema do jogo
-;; - estado-inicial: contem o estado inicial do problema de procura;
-;; - solucao: funcao que recebe um estado e devolve T se o estado
-;;     for uma solucao para o problema de procura, e nil caso contrario;
-;; - accoes: funcao que recebe um estado e devolve uma lista com todas
-;;     as accoes que sao possiveis fazer nesse estado;
-;; - resultado: funcao que dado um estado e uma accao devolve o estado
-;;     sucessor que resulta de executar a accao recebida no estado recebido;
-;; - custo-caminho: funcao que dado um estado devolve o custo do caminho
-;;     desde o estado inicial ate esse estado.
 (defstruct problema
+  "Estrutura do problema do jogo
+- estado-inicial: contem o estado inicial do problema de procura;
+- solucao: funcao que recebe um estado e devolve T se o estado
+    for uma solucao para o problema de procura, e nil caso contrario;
+- accoes: funcao que recebe um estado e devolve uma lista com todas
+    as accoes que sao possiveis fazer nesse estado;
+- resultado: funcao que dado um estado e uma accao devolve o estado
+    sucessor que resulta de executar a accao recebida no estado recebido;
+- custo-caminho: funcao que dado um estado devolve o custo do caminho
+    desde o estado inicial ate esse estado."
   estado-inicial
   solucao
   accoes
   resultado
   custo-caminho)
 
-;;	solucao: estado -> logico
-;;	Esta funcao recebe um estado, e devolve o valor logico verdade se o estado
-;;recebido corresponder a uma solucao, e falso contrario. Um estado do jogo
-;;Tetris e considerado solucao se o topo do tabuleiro nao estiver preenchido e
-;;se ja nao existem pecas por colocar, ou seja, todas as pecas foram colocadas
-;;com sucesso.
 (defun solucao (estado)
+	"solucao: estado -> logico
+	Esta funcao recebe um estado, e devolve o valor logico verdade se o estado
+recebido corresponder a uma solucao, e falso contrario. Um estado do jogo
+Tetris e considerado solucao se o topo do tabuleiro nao estiver preenchido e
+se ja nao existem pecas por colocar, ou seja, todas as pecas foram colocadas
+com sucesso."
   (let* ((tabuleiro  (estado-tabuleiro            estado))
          (pecas      (estado-pecas-por-colocar    estado))
          (preenchido (tabuleiro-topo-preenchido-p tabuleiro)))
     (and (not preenchido)
          (null pecas))))
 
-;;	accoes: estado -> lista de accoes
-;;	Esta funcao recebe um estado e devolve uma lista de accoes correspondendo a
-;;todas as accoes validas que podem ser feitas com a proxima peca a ser
-;;colocada. Uma accao e considerada valida mesmo que faca o jogador perder o
-;;jogo.
 (defun accoes (estado)
+	"accoes: estado -> lista de accoes
+	Esta funcao recebe um estado e devolve uma lista de accoes correspondendo a
+todas as accoes validas que podem ser feitas com a proxima peca a ser
+colocada. Uma accao e considerada valida mesmo que faca o jogador perder o
+jogo."
   (let ((lista-accoes nil)
         (numero-de-colunas 10))
     (labels ((accoes-validas (peca)
@@ -54,10 +54,10 @@
         (t (map 'list #'accoes-validas (list peca-t0 peca-t1 peca-t2 peca-t3))))
       lista-accoes)))
 
-;;  resultado: estado x accao -> estado
-;;	Esta funcao recebe um estado e uma accao, e devolve um novo estado que
-;;  resulta de aplicar a accao recebida no estado original.
 (defun resultado (estado accao)
+  "resultado: estado x accao -> estado
+	Esta funcao recebe um estado e uma accao, e devolve um novo estado que
+ resulta de aplicar a accao recebida no estado original."
   (let* ((novo-estado (copia-estado estado))
          (letra       (first (estado-pecas-por-colocar novo-estado)))
          (tabuleiro   (estado-tabuleiro novo-estado))
@@ -90,18 +90,19 @@ Dada uma peca, devolve a letra que representa a peca."
         ((member peca (list peca-z0 peca-z1)                 :test #'equalp) 'z)
         ((member peca (list peca-t0 peca-t1 peca-t2 peca-t3) :test #'equalp) 't)))
 
-;;	qualidade: estado -> inteiro
-;;	Esta funcao recebe um estado e retorna um valor de quatidade que corresponde ao
-;;valor negativo dos pontos ganhos ate ao momento.
 (defun qualidade (estado)
+	"qualidade: estado -> inteiro
+	Esta funcao recebe um estado e retorna um valor de quatidade que corresponde ao
+valor negativo dos pontos ganhos ate ao momento."
   (* -1 (estado-pontos estado)))
 
-;;	custo-oportunidade: estado -> inteiro
-;;	Esta funcao, dado um estado, devolve o custo de oportunidade de todas as accoes
-;;realizadas ate ao momento, assumindo que e sempre possivel fazer o maximo de pontos
-;;por cada peca colocada
 (defun custo-oportunidade (estado)
-  (- (pontos-maximos(estado-pecas-colocadas estado)) (estado-pontos estado)))
+	"custo-oportunidade: estado -> inteiro
+	Esta funcao, dado um estado, devolve o custo de oportunidade de todas as accoes
+realizadas ate ao momento, assumindo que e sempre possivel fazer o maximo de pontos
+por cada peca colocada"
+  (- (pontos-maximos (estado-pecas-colocadas estado))
+     (estado-pontos estado)))
 
 (defun pontos-maximos (lis)
   (cond ((equal (first lis) 'i)
