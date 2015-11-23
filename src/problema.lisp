f163e165d42eab54997c1d9ffa13d0d5fdf6860f
rodamber <rodrigoambernardo@gmail.com>
2015-11-23 22:43:12 +0000

    Changed function accoes according to Errata

--- src/problema.lisp
+++ src/problema.lisp
@@ -18,8 +18,8 @@
   custo-caminho)
 
 (defun solucao (estado)
-	"solucao: estado -> logico
-	Esta funcao recebe um estado, e devolve o valor logico verdade se o estado
+  "solucao: estado -> logico
+Esta funcao recebe um estado, e devolve o valor logico verdade se o estado
 recebido corresponder a uma solucao, e falso contrario. Um estado do jogo
 Tetris e considerado solucao se o topo do tabuleiro nao estiver preenchido e
 se ja nao existem pecas por colocar, ou seja, todas as pecas foram colocadas
@@ -31,33 +31,39 @@ com sucesso."
          (null pecas))))
 
 (defun accoes (estado)
-	"accoes: estado -> lista de accoes
-	Esta funcao recebe um estado e devolve uma lista de accoes correspondendo a
+  "accoes: estado -> lista de accoes
+Esta funcao recebe um estado e devolve uma lista de accoes correspondendo a
 todas as accoes validas que podem ser feitas com a proxima peca a ser
 colocada. Uma accao e considerada valida mesmo que faca o jogador perder o
 jogo."
-  (let ((lista-accoes nil)
-        (numero-de-colunas 10))
-    (labels ((accoes-validas (peca)
-                  (let* ((largura         (array-dimension peca 1))
-                         (colunas-validas (- (1+ numero-de-colunas) largura)))
-                    (dotimes (i colunas-validas)
-                      (setf lista-accoes
-                            (append lista-accoes (list (cria-accao i peca))))))))
-      (case (first (estado-pecas-por-colocar estado))
-        (i (map 'list #'accoes-validas (list peca-i0 peca-i1)))
-        (l (map 'list #'accoes-validas (list peca-l0 peca-l1 peca-l2 peca-l3)))
-        (o (map 'list #'accoes-validas (list peca-o0)))
-        (s (map 'list #'accoes-validas (list peca-s0 peca-s1)))
-        (z (map 'list #'accoes-validas (list peca-z0 peca-z1)))
-        (j (map 'list #'accoes-validas (list peca-j0 peca-j1 peca-j2 peca-j3)))
-        (t (map 'list #'accoes-validas (list peca-t0 peca-t1 peca-t2 peca-t3))))
-      lista-accoes)))
+  (if (estado-final-p estado)
+      nil
+      (let ((lista-accoes nil)
+            (numero-de-colunas 10))
+        (labels ((accoes-validas (peca)
+                   (let* ((largura (array-dimension peca
+                                                    1))
+                          (colunas-validas (- (1+ numero-de-colunas)
+                                              largura)))
+                     (dotimes (i colunas-validas)
+                       (setf lista-accoes
+                             (append lista-accoes
+                                     (list (cria-accao i
+                                                       peca))))))))
+          (case (first (estado-pecas-por-colocar estado))
+            (i (map 'list #'accoes-validas (list peca-i0 peca-i1)))
+            (l (map 'list #'accoes-validas (list peca-l0 peca-l1 peca-l2 peca-l3)))
+            (o (map 'list #'accoes-validas (list peca-o0)))
+            (s (map 'list #'accoes-validas (list peca-s0 peca-s1)))
+            (z (map 'list #'accoes-validas (list peca-z0 peca-z1)))
+            (j (map 'list #'accoes-validas (list peca-j0 peca-j1 peca-j2 peca-j3)))
+            (t (map 'list #'accoes-validas (list peca-t0 peca-t1 peca-t2 peca-t3))))
+          lista-accoes))))
 
 (defun resultado (estado accao)
   "resultado: estado x accao -> estado
-	Esta funcao recebe um estado e uma accao, e devolve um novo estado que
- resulta de aplicar a accao recebida no estado original."
+Esta funcao recebe um estado e uma accao, e devolve um novo estado que
+resulta de aplicar a accao recebida no estado original."
   (let* ((novo-estado (copia-estado estado))
          (letra       (first (estado-pecas-por-colocar novo-estado)))
          (tabuleiro   (estado-tabuleiro novo-estado))
@@ -91,14 +97,14 @@ Dada uma peca, devolve a letra que representa a peca."
         ((member peca (list peca-t0 peca-t1 peca-t2 peca-t3) :test #'equalp) 't)))
 
 (defun qualidade (estado)
-	"qualidade: estado -> inteiro
-	Esta funcao recebe um estado e retorna um valor de quatidade que corresponde ao
+  "qualidade: estado -> inteiro
+Esta funcao recebe um estado e retorna um valor de quatidade que corresponde ao
 valor negativo dos pontos ganhos ate ao momento."
   (* -1 (estado-pontos estado)))
 
 (defun custo-oportunidade (estado)
-	"custo-oportunidade: estado -> inteiro
-	Esta funcao, dado um estado, devolve o custo de oportunidade de todas as accoes
+  "custo-oportunidade: estado -> inteiro
+  Esta funcao, dado um estado, devolve o custo de oportunidade de todas as accoes
 realizadas ate ao momento, assumindo que e sempre possivel fazer o maximo de pontos
 por cada peca colocada"
   (- (pontos-maximos (estado-pecas-colocadas estado))
