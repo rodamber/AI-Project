982247b8b3a8e43484cceedcc5ee6fad30f2fa57
rodamber <rodrigoambernardo@gmail.com>
2015-11-24 15:03:22 +0000

    Version of procura-pp with no explored set

--- src/procura-pp.lisp
+++ src/procura-pp.lisp
@@ -1,3 +1,4 @@
+;;; Versao com conjunto de estados ja explorados.
 (defun procura-pp (problema)
   "procura-pp: problema --> lista de accoes"
   (let ((estados-explorados ())
@@ -41,5 +42,39 @@
                                  (return-from procura-pp-recursivo
                                    (cons accao procura)))))))))
                  'INSUCESSO)))
+          (procura-pp-recursivo estado-inicial)))))
+
+;;; Versao do procura-pp sem manter conjunto de estados ja explorados.
+(defun procura-pp-2 (problema)
+  "procura-pp: problema --> lista de accoes"
+  (let ((estado-inicial (problema-estado-inicial problema))
+        (solucao        (problema-solucao        problema))
+        (accoes         (problema-accoes         problema))
+        (resultado      (problema-resultado      problema)))
+    (if (funcall solucao estado-inicial)
+        nil
+        (labels
+            ((procura-pp-recursivo (estado)
+               (let ((lista-accoes (reverse (funcall accoes estado))))
+                 (if (null lista-accoes)
+                     (return-from procura-pp-recursivo
+                       'INSUCESSO)
+                     ;; Para cada accao possivel, por ordem LIFO, expande o
+                     ;; estado e verifica se e solucao na geracao. Se for
+                     ;; solucao devolve uma lista com a accao que o originou;
+                     ;; senao, verifica se ja tinha sido explorado. Se sim,
+                     ;; devolve insucesso; se nao, realiza uma procura a
+                     ;; partir desse estado.
+                     (dolist (accao lista-accoes 'INSUCESSO)
+                       (let ((novo-estado (funcall resultado
+                                                   estado
+                                                   accao)))
+                         (when (funcall solucao novo-estado)
+                           (return-from procura-pp-recursivo
+                             (list accao)))
+                         (let ((procura (procura-pp-recursivo novo-estado)))
+                           (when (not (equalp procura 'INSUCESSO))
+                             (return-from procura-pp-recursivo
+                               (cons accao procura))))))))))
           (procura-pp-recursivo estado-inicial)))))
 
