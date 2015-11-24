564da6ab383a2dd418bf6d6b257e6f1f8a1c9d1c
rodamber <rodrigoambernardo@gmail.com>
2015-11-24 19:04:59 +0000

    Refactored heuristics.
    Heuristics now return 0 on goal state. Added
    heuristica-pecas-por-colocar.

--- src/searches/heuristicas.lisp
+++ src/searches/heuristicas.lisp
@@ -1,19 +1,50 @@
 (defun heuristica-relevo (estado)
-	(let ((pontuacao 0)
-		  (tabuleiro (estado-tabuleiro estado)))
-		(loop for coluna from 1 upto 9 do
-			(incf pontuacao 
-				(abs (- (tabuleiro-altura-coluna tabuleiro coluna) 
-						(tabuleiro-altura-coluna tabuleiro (1- coluna))))))
-		pontuacao))
+  "heuristica-relevo: estado --> numero"
+  (if (solucao estado)
+      0
+      (~heuristica-relevo estado)))
 
 (defun heuristica-buracos (estado)
-	(let ((pontuacao 0)
-		  (tabuleiro (estado-tabuleiro estado)))
-		(dotimes (coluna 10)
-			(loop for linha from (1- (tabuleiro-altura-coluna tabuleiro coluna)) downto 0 do
-				(if (not (tabuleiro-preenchido-p tabuleiro linha coluna)) (incf pontuacao))))
-		pontuacao))
+  "heuristica-buracos: estado --> numero"
+  (if (solucao estado)
+      0
+      (~heuristica-buracos estado)))
 
-(defun heuristicas-pontuacao (estado)
-	(estado-pontos estado))
+(defun heuristica-pontuacao (estado)
+  "heuristica-pontuacao: estado --> numero"
+  (if (solucao estado)
+      0
+      (~heuristica-pontuacao estado)))
+
+(defun heuristica-pecas-por-colocar (estado)
+  "heuristica-pecas-por-colocar: estado --> numero"
+  (if (solucao estado)
+      0
+      (~heuristica-pecas-por-colocar estado)))
+
+;-------------------------------------------------------------------------------
+
+(defun ~heuristica-relevo (estado)
+  "~heuristica-relevo: estado --> numero"
+  (let ((relevo 0)
+        (tabuleiro (estado-tabuleiro estado)))
+    (loop for coluna from 1 upto 9 do
+      (let ((a1 (tabuleiro-altura-coluna tabuleiro coluna))
+            (a2 (tabuleiro-altura-coluna tabuleiro (1- coluna))))
+        (incf relevo (abs (- a1 a2)))))
+    relevo))
+
+(defun ~heuristica-buracos (estado)
+  (let ((buracos 0)
+        (tabuleiro (estado-tabuleiro estado)))
+    (dotimes (coluna 10 buracos)
+      (incf buracos
+            (tabuleiro-coluna-buracos tabuleiro
+                                      coluna)))))
+
+(defun ~heuristica-pontuacao (estado)
+  (estado-pontos estado))
+
+(defun ~heuristica-pecas-por-colocar (estado)
+  (estado-pecas-por-colocar estado))
+
