73f6345bc57f0b64eb9699d83569fea6e1dbeb43
rodamber <rodrigoambernardo@gmail.com>
2015-11-26 23:56:06 +0000

    A* with pruning.

--- src/searches/heuristicas.lisp
+++ src/searches/heuristicas.lisp
@@ -67,11 +67,12 @@ devolve o resultado de aplicar a funcao quase heuristica ao estado recebido."
             (tabuleiro-altura-coluna tabuleiro
                                      coluna)))))
 
-(defun h-rbppa (estado)
+(defun heuristica-arpb (estado)
   (labels
       ((~h (estado)
-         (+ (funcall #'~heuristica-altura-agregada   estado)
-            (funcall #'~heuristica-pecas-por-colocar estado)
-            (funcall #'~heuristica-pontuacao         estado)
-            (funcall #'~heuristica-buracos           estado))))
+         (* 1000
+            (+ (funcall #'~heuristica-altura-agregada estado)
+               (funcall #'~heuristica-relevo          estado)
+               (funcall #'~heuristica-pontuacao       estado)
+               (funcall #'~heuristica-buracos         estado)))))
     (funcall #'heuristica-auxiliar #'~h estado)))
--- src/searches/procura-best.lisp
+++ src/searches/procura-best.lisp
@@ -10,6 +10,45 @@
                                   :custo-caminho  #'custo-oportunidade)))
     (procura-best-problema problema)))
 
+(defun poda (fronteira n)
+  (if (>= n (list-length fronteira))
+      fronteira
+      (subseq fronteira 0 n)))
+
 (defun procura-best-problema (problema)
   "procura-best-problema: problema --> lista de accoes"
-  (procura-A* problema)) ; Aqui, nao tera que ser, necessariamente, a procura A*
+  (let* ((estado-inicial (problema-estado-inicial problema))
+         (solucao        (problema-solucao        problema))
+         (accoes         (problema-accoes         problema))
+         (resultado      (problema-resultado      problema))
+         (g              (problema-custo-caminho  problema))
+         (h #'heuristica-arpb)
+         (fronteira (list (make-no :estado          estado-inicial
+                                   :avaliacao       0
+                                   :caminho nil))))
+    (loop do
+      (dotimes (i 10)
+        (let ((no (pop fronteira)))
+          (cond ((null no)
+                 (return-from procura-best-problema
+                   nil))
+                ((funcall solucao (no-estado no))
+                 (return-from procura-best-problema
+                   (reverse (no-caminho no))))
+                (t
+                 ;; Expande o no e adiciona os nos resultantes a fronteira
+                 ;; ordenados pela funcao de avaliacao f(n) = g(n) + h(n).
+                 (let* ((estado (no-estado no))
+                        (lista-accoes (funcall accoes estado)))
+                   (dolist (accao lista-accoes)
+                     (let* ((novo-estado (funcall resultado estado accao))
+                            (novo-no (make-no :estado novo-estado
+                                              :avaliacao (+ (funcall g novo-estado)
+                                                            (funcall h novo-estado))
+                                              :caminho (cons accao
+                                                             (no-caminho no)))))
+                       ;; Insere o no na fronteira.
+                       (setf fronteira
+                             (merge 'list (list novo-no) fronteira #'no<)))))))))
+      (setf fronteira
+            (poda fronteira 5)))))
