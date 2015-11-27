ebc72a65a8c4ed905f4f9f25b863cb6f190d40a3
rodamber <rodrigoambernardo@gmail.com>
2015-11-27 00:37:09 +0000

    Improvement on PP

--- src/searches/procura-pp.lisp
+++ src/searches/procura-pp.lisp
@@ -8,26 +8,24 @@
         ((procura-pp-recursivo (estado)
            "procura-pp-recursivo: estado --> lista de accoes"
            (let ((lista-accoes (reverse (funcall accoes estado))))
-             (if (null lista-accoes)
-                 nil
-                 ;; Para cada accao possivel, por ordem LIFO, expande o
-                 ;; estado e verifica se e solucao na geracao. Se for
-                 ;; solucao devolve uma lista com a accao que a originou; se
-                 ;; nao, comeca uma procura a partir desse estado.
-                 (dolist (accao lista-accoes nil)
-                   (let ((novo-estado (funcall resultado estado accao)))
-                     (when (funcall solucao novo-estado)
-                       (return-from procura-pp-recursivo
-                         (list accao)))
-                     (let ((procura (procura-pp-recursivo novo-estado)))
-                       ;; Se encontrarmos solucao a partir do estado gerado,
-                       ;; juntamos a lista de accoes *devolvida* pela procura a
-                       ;; accao que gerou esse estado. Se o resultado da procura
-                       ;; for vazio, entao a procura nao encontrou solucao e
-                       ;; procede-se a geracao do estado resultante da aplicacao
-                       ;; da proxima accao na *lista-accoes*.
-                       (when (not (null procura))
-                         (return-from procura-pp-recursivo
-                           (cons accao procura))))))))))
+             ;; Para cada accao possivel, por ordem LIFO, expande o
+             ;; estado e verifica se e solucao na geracao. Se for
+             ;; solucao devolve uma lista com a accao que a originou; se
+             ;; nao, comeca uma procura a partir desse estado.
+             (dolist (accao lista-accoes nil)
+               (let ((novo-estado (funcall resultado estado accao)))
+                 (when (funcall solucao novo-estado)
+                   (return-from procura-pp-recursivo
+                     (list accao)))
+                 (let ((procura (procura-pp-recursivo novo-estado)))
+                   ;; Se encontrarmos solucao a partir do estado gerado,
+                   ;; juntamos a lista de accoes *devolvida* pela procura a
+                   ;; accao que gerou esse estado. Se o resultado da procura
+                   ;; for vazio, entao a procura nao encontrou solucao e
+                   ;; procede-se a geracao do estado resultante da aplicacao
+                   ;; da proxima accao na *lista-accoes*.
+                   (when (not (null procura))
+                     (return-from procura-pp-recursivo
+                       (cons accao procura)))))))))
       (procura-pp-recursivo estado-inicial))))
 
